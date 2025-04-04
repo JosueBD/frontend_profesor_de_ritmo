73c1021ba57c140fee5059d84fd533aef84ab156
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-04-04 19:04:54 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './i18n'; // ✅ Importa el sistema de traducción ANTES de cargar la App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
