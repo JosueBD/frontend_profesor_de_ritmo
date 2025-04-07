4f8f6e3a032c26d8952abacae2c090ecf67e49a4
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-04-06 22:31:42 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import './i18n'; // ✅ Importa el sistema de traducción ANTES de cargar la App
-import EditorPartitura from "./EditorPartitura";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "./i18n";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
