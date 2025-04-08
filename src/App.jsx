c09197583bf8d96c63a42f630473933772ecddea
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-04-08 17:26:04 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom"; // ⬅️ FALTA ESTO
 import SidebarLayout from "./layouts/SidebarLayout";
+
+import VistaPrincipal from "./vistas/VistaPrincipal";
+import Editor from "./vistas/Editor";
+import Api from "./vistas/Api";
+import Ritmo from "./vistas/Ritmo";
+import VistaGrabacionAudio from "./VistaGrabacionAudio";
 
 function App() {
   return (
