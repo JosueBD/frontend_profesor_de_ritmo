eab87215cf36cf2c891a3840861309c6e1f63e17
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-04-09 00:12:57 -0400

    Create PartituraViewer.jsx

---
+++ src/componentes/PartituraViewer.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from "react";
+import { OpenSheetMusicDisplay } from "opensheetmusicdisplay";
+
+const PartituraViewer = ({ musicXML }) => {
+  const containerRef = useRef(null);
+  const osmdRef = useRef(null);
+
+  useEffect(() => {
+    if (musicXML && containerRef.current) {
+      if (!osmdRef.current) {
+        osmdRef.current = new OpenSheetMusicDisplay(containerRef.current, {
+          autoResize: true,
+          drawTitle: false,
+        });
+      }
+      osmdRef.current
+        .load(musicXML)
+        .then(() => osmdRef.current.render())
+        .catch((err) => console.error("Error al renderizar:", err));
+    }
+  }, [musicXML]);
+
+  return <div ref={containerRef} style={{ width: "100%" }} />;
+};
+
+export default PartituraViewer;
