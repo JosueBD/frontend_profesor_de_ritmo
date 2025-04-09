60886adf3a36f2eea27b79eddff33c026e5e9857
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-04-08 21:33:01 -0400

    Delete src/build.sh

--- src/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# 🔁 Actualizar repositorios e instalar Lilypond
-apt-get update && apt-get install -y lilypond
-
-# 🔧 Configurar la ruta de Lilypond para music21
-python -c "from music21 import environment; environment.UserSettings()['lilypondPath'] = '/usr/bin/lilypond'"
-
-echo "✅ Lilypond instalado y configurado para music21"
