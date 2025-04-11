189e5a6bc8ae0e2f95df4fe47460bb1c089f5cdd
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-04-10 23:36:09 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-import "./i18n.js";
-import { BrowserRouter } from "react-router-dom";
-
-// 👇 FORZAR idioma español si no hay uno guardado
-if (!localStorage.getItem("i18nextLng")) {
-  localStorage.setItem("i18nextLng", "es");
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
